f1fd38650a8e85644b775ce75b500ce70723feb1
sumedhjoshi <sumedh.m.joshi@gmail.com>
2015-07-13 16:48:48 -0500

    Updated the visualization script to generate DJL-appropriate figures.

--- SMPM-inceuler/viz_djl_four_methods.m
+++ SMPM-inceuler/viz_djl_four_methods.m
@@ -6,30 +6,30 @@
    method = { 'Poisson', 'Post-Projection', 'Nullspace-Projection', 'Post-Weak-Projection' };
 
    % Set some filenames in order.
-   fnames = { 'raytay_poisson.mat', 'raytay_postproject.mat', 'raytay_nullspace.mat', 'raytay_postnull.mat' };
+   fnames = { 'djl_poisson.mat', 'djl_postproject.mat', 'djl_nullspace.mat', 'djl_postnull.mat' };
 
-   % Set the visualization window.
-   win = [ 1 / 2 - 0.2 * 1,  1 / 2 + 0.2 * 1 ];
-%   win = [ 0, 1 ];
+   % Set the visualization window in x.
+   win = [ 1, 4 ];
 
    % Set the time you want to extract.
-   tplot = 0.85;
+   tplot = 0.5;
 
    % Figure 1: quiver plot of the final velocity.
    figure;
    for iiviz = 1:length(method)
 
-
       load( fnames{iiviz} );
+      x = reshape( x, n * mz, n * mx );
+      z = reshape( z, n * mz, n * mx );
       [junk ndx] = min( abs( t - tplot ) );
       subplot( 2, 2, iiviz );
-      quiver( x, z, ux(:,ndx), uz(:,ndx), 5.0 );
-      set( gca, 'xlim', win, 'ylim', win );
+      quiver( x, z, ux(:,:,ndx), uz(:,:,ndx), 1.0 );
+      set( gca, 'xlim', win );
       title( [ method{iiviz} ': velocity' ] );
 
    end
    set( gcf,'pos',[2     5   948   690]);
-   print_graphics( gcf, 'compare_velocity', 1, 0, 0, 0 );
+   print_graphics( gcf, 'compare_djl_velocity', 1, 0, 0, 0 );
 
    % Figure 2: mass conservation.
    figure;
@@ -38,7 +38,7 @@
 
       load( fnames{iiviz} );
       [ junk ndx ] = min( abs( t - tplot ) );
-      mass = integrate_grid_function( rho, x, z, n, mx, mz );
+      mass = integrate_grid_function( reshape( rho, n * n * mx * mz, length(t) ), x, z, n, mx, mz );
       plot( t(1:ndx), mass(1:ndx) );
 
    end
@@ -46,13 +46,15 @@
    ylabel( 'total mass' );
    box on;
    legend( method, 'Location', 'SouthEast' );
-   print_graphics( gcf, 'compare_mass', 1, 0, 0, 0 );
+   print_graphics( gcf, 'compare_djl_mass', 1, 0, 0, 0 );
 
    % Figure 3: continuity.
    figure;
    for iiviz = 1:length(method)
 
       load( fnames{iiviz} );
+      ux = reshape( ux, r, length(t) );
+      uz = reshape( uz, r, length(t) );
       [junk ndx] = min( abs( t - tplot ) );
       c = sqrt( sum( ( E0 * ux(:,1:ndx) ).^2 + ( E0 * uz(:,1:ndx) ).^2 ) );
       semilogy( t(1:ndx), c(1:ndx) );
@@ -63,7 +65,7 @@
    ylabel( 'C^0 discontinuity norm' );
    legend( method , 'Location', 'SouthEast' );
    box on;
-   print_graphics( gcf, 'compare_continuity', 1, 0, 0, 0 );
+   print_graphics( gcf, 'compare_djl_continuity', 1, 0, 0, 0 );
 
    % Figure 4: divergence.
    figure;
@@ -71,6 +73,8 @@
 
       load( fnames{iiviz} );
       [ junk ndx ] = min( abs( t - tplot ) );
+      ux = reshape( ux, r, length(t) );
+      uz = reshape( uz, r, length(t) );
       divu = sqrt( sum( ( D * [ux(:,1:ndx); uz(:,1:ndx)]).^2 ) );
       semilogy( t(1:ndx), divu );
       hold all;
@@ -80,7 +84,7 @@
    ylabel( 'norm of divergence' );
    legend( method , 'Location', 'SouthEast' );
    box on;
-   print_graphics( gcf, 'compare_divergence', 1, 0, 0, 0 );
+   print_graphics( gcf, 'compare_djl_divergence', 1, 0, 0, 0 );
 
    % Figure 5: contour plot of the final density
    figure;
@@ -91,20 +95,25 @@
       load( fnames{iiviz} );
       subplot( 2, 2, iiviz );
       [ junk ndx ] = min( abs( t - tplot ) );
-      contourf( reshape(x, n * mx, n * mz ), reshape( z, n * mx, n * mz  ), 1000.0 + reshape( rho(:,ndx), n * mx, n * mz ), 25 )
-      set( gca, 'xlim', win, 'ylim', win );
+      rhob = reshape( rhob, n * mz , n * mx );
+      x = reshape( x, n * mz, n * mx );
+      z = reshape( z, n * mz, n * mx );
+      contourf( x, z, rhob + rho(:,:,ndx), 25 );
+      set( gca, 'xlim', win );
       title( [ method{iiviz} ': density' ] );
       colorbar;
 
    end
    set( gcf,'pos',[2     5   948   690]);
-   print_graphics( gcf, 'compare_density', 1, 0, 0, 0 );
+   print_graphics( gcf, 'compare_djl_density', 1, 0, 0, 0 );
 
    % Figure 6: show norm of velocity field to get a sense of instability.
    figure;
    for iiviz = 1:length(method)
 
       load( fnames{iiviz} );
+      ux = reshape( ux, r, length(t) );
+      uz = reshape( uz, r, length(t) );
       normu = sqrt( sum( ux.^2 + uz.^2 ) );
       semilogy( t, normu );
       hold all;
@@ -114,4 +123,4 @@
    ylabel( 'norm of velocity' );
    legend( method , 'Location', 'SouthEast' );
    box on;
-   print_graphics( gcf, 'compare_velocity_norm', 1, 0, 0, 0 );
+   print_graphics( gcf, 'compare_djl_velocity_norm', 1, 0, 0, 0 );
